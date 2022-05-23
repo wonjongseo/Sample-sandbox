3728813fc69c6b8dbbc50d74515813f962da0c38
wonjongseo <63527605+wonjongseo@users.noreply.github.com>
2022-05-23 22:41:30 +0900

    Setter state prev value

--- src/App.js
+++ src/App.js
@@ -1,20 +1,10 @@
 import React from "react";
 import "./styles.css";
 
-const Title = () => <h3>Total Click : 0</h3>;
-const Button = () => <button>Click Me</button>;
-const Container = () => {
-  return (
-    <div>
-      <Title />
-      <Button />
-    </div>
-  );
-};
 export default () => {
   const [counter, setCounter] = React.useState(0);
   const onClick = () => {
-    setCounter(counter + 1);
+    setCounter((prev) => prev + 1);
   };
   return (
     <div>
