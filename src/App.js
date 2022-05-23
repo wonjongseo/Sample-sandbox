1e2936061647cf67c3ee1eea2e350a360118506e
wonjongseo <63527605+wonjongseo@users.noreply.github.com>
2022-05-23 23:30:11 +0900

    Time Converter

--- src/App.js
+++ src/App.js
@@ -2,14 +2,60 @@ import React from "react";
 import "./styles.css";
 
 export default () => {
-  const [counter, setCounter] = React.useState(0);
-  const onClick = () => {
-    setCounter((prev) => prev + 1);
+  const [minutes, setMinutes] = React.useState(0);
+  const [flip, setFlip] = React.useState(false);
+  const onChange = (event) => {
+    setMinutes(event.target.value);
+  };
+  const reset = () => {
+    setMinutes(0);
   };
   return (
-    <div>
-      <h3>Total Count {counter}</h3>
-      <button onClick={onClick}>Click me!</button>
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        height: "100vh",
+        flexDirection: "column"
+      }}
+    >
+      <h1 className="converter" style={{ marginBottom: "30px" }}>
+        Converter
+      </h1>
+      <div>
+        <label htmlFor="minutes">Minutes</label>
+        <input
+          onChange={onChange}
+          value={flip ? minutes * 60 : minutes}
+          id="minutes"
+          placeholder="Minutes"
+          type="number"
+          disabled={flip}
+        />
+      </div>
+      <div>
+        <label htmlFor="hours">Hours</label>
+        <input
+          value={flip ? minutes : Math.floor(minutes / 60)}
+          id="hours"
+          placeholder="Houres"
+          type="number"
+          disabled={!flip}
+          onChange={onChange}
+        />
+      </div>
+      <div>
+        <button onClick={reset}>Reset</button>
+        <button
+          onClick={() => {
+            reset();
+            setFlip((prev) => !prev);
+          }}
+        >
+          Flip
+        </button>
+      </div>
     </div>
   );
 };
