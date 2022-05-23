47f9a6fdda4b131ce88586eb2e18484de541b54e
wonjongseo <63527605+wonjongseo@users.noreply.github.com>
2022-05-23 21:24:26 +0900

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h3 id="title" onMouseEnter={() => alert("Hello")}>
+        Hello
+      </h3>
+      <h1>Hello CodeSandbox</h1>
+      <h2>Start editing to see some magic happen!</h2>
+    </div>
+  );
+}
