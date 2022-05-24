5bf3eec3b0c2da28141276251b65999d30d19f47
wonjongseo <63527605+wonjongseo@users.noreply.github.com>
2022-05-24 11:10:52 +0900

    .module.css css　파일을 자바스크립트 객체 처럼 사용할 수 있다. React　개발의 devide and Counqur 을 용이하게 해줌

--- src/App.js
+++ src/App.js
@@ -1,61 +1,14 @@
+//npx create-react-app@5.0.0 my-app
+
 import React from "react";
-import "./styles.css";
+import Button from "./Button";
+import styles from "./App.module.css";
 
 export default () => {
-  const [minutes, setMinutes] = React.useState(0);
-  const [flip, setFlip] = React.useState(false);
-  const onChange = (event) => {
-    setMinutes(event.target.value);
-  };
-  const reset = () => {
-    setMinutes(0);
-  };
   return (
-    <div
-      style={{
-        display: "flex",
-        justifyContent: "center",
-        alignItems: "center",
-        height: "100vh",
-        flexDirection: "column"
-      }}
-    >
-      <h1 className="converter" style={{ marginBottom: "30px" }}>
-        Converter
-      </h1>
-      <div>
-        <label htmlFor="minutes">Minutes</label>
-        <input
-          onChange={onChange}
-          value={flip ? minutes * 60 : minutes}
-          id="minutes"
-          placeholder="Minutes"
-          type="number"
-          disabled={flip}
-        />
-      </div>
-      <div>
-        <label htmlFor="hours">Hours</label>
-        <input
-          value={flip ? minutes : Math.floor(minutes / 60)}
-          id="hours"
-          placeholder="Houres"
-          type="number"
-          disabled={!flip}
-          onChange={onChange}
-        />
-      </div>
-      <div>
-        <button onClick={reset}>Reset</button>
-        <button
-          onClick={() => {
-            reset();
-            setFlip((prev) => !prev);
-          }}
-        >
-          Flip
-        </button>
-      </div>
+    <div>
+      <h1 className={styles.title}>Welcome to React!!</h1>
+      <Button text="Helo" />
     </div>
   );
 };
