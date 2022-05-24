1b0d009ecfd5194200662f062c0d4009a73ea35d
wonjongseo <63527605+wonjongseo@users.noreply.github.com>
2022-05-24 16:20:00 +0900

    toDo list

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
 //npx create-react-app@5.0.0 my-app
 
 import React from "react";
-import Button from "./Button";
-import styles from "./App.module.css";
 
 export default () => {
+  const [toDo, setToDo] = React.useState("");
+  const [toDos, setToDos] = React.useState([]);
+  const onChange = (event) => setToDo(event.target.value);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    if (toDo === "") return;
+    setToDos((prev) => [toDo, ...prev]);
+    setToDo("");
+  };
+
   return (
     <div>
-      <h1 className={styles.title}>Welcome to React!!</h1>
-      <Button text="Helo" />
+      <h1>My To Dos ({toDos.length}) </h1>
+      <form onSubmit={onSubmit}>
+        <input
+          type="text"
+          value={toDo}
+          onChange={onChange}
+          placeholder="Input your Doto "
+        />
+        <button>Add To do</button>
+      </form>
+      <hr />
+      <ul>
+        {toDos.map((toDo, index) => (
+          <li key={index}>
+            {index + 1}: {toDo}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
